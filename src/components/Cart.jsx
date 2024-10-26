cbc8d27be77b607335ed9f167c6ca753f3861f5d
Do Nam Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2024-10-26 10:34:30 +0700

    Style cart view

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,24 +5,59 @@ import styles from "../styles/Cart.module.css";
 export default function Cart() {
   const [shopItems, cart, setCart] = useOutletContext();
 
-  console.log("Cart: ", cart);
+  const handleCartChange = (itemId, type) => {
+    setCart((prevCart) =>
+      prevCart
+        .map((cartItem) =>
+          cartItem.item.id === itemId
+            ? {
+                ...cartItem,
+                count: type === "add" ? cartItem.count + 1 : cartItem.count - 1,
+              }
+            : cartItem
+        )
+        .filter((cartItem) => cartItem.count > 0)
+    );
+  };
 
   return (
-    <div>
-      {cart
-        ? cart.map((cartItem) => {
-            const { item } = cartItem;
-            return (
-              <div key={item.id}>
-                <img src={item.image} alt="" />
-                <div>{item.title}</div>
-                <div>
-                  {item.price} Amount: {cartItem.count}
+    <div className={styles.cartView}>
+      <div className={styles.header}>
+        <h2>Your Cart</h2>
+        <h2>
+          Total: $
+          {Math.round(
+            cart.reduce(
+              (total, cartItem) => total + cartItem.item.price * cartItem.count,
+              0
+            ) * 100
+          ) / 100}
+        </h2>
+      </div>
+
+      <div className={styles.cartList}>
+        {cart
+          ? cart.map((cartItem) => {
+              const { item } = cartItem;
+              return (
+                <div key={item.id} className={styles.cartDisplay}>
+                  <img src={item.image} alt="" />
+                  <div className={styles.title}>{item.title}</div>
+                  <div>${item.price}</div>
+                  <div className={styles.changeAmount}>
+                    <button onClick={() => handleCartChange(item.id, "remove")}>
+                      &lt;
+                    </button>
+                    <div>{cartItem.count}</div>
+                    <button onClick={() => handleCartChange(item.id, "add")}>
+                      &gt;
+                    </button>
+                  </div>
                 </div>
-              </div>
-            );
-          })
-        : ""}
+              );
+            })
+          : ""}
+      </div>
     </div>
   );
 }
