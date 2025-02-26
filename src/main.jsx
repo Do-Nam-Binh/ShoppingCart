1407f5303d313f0fd744e8519365aa7cd8bb363c
Do-Nam-Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2025-02-26 16:19:32 +0700

    Update main.jsx
    Change to HashRouter to be compatible with github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App";
 import Shop from "./components/Shop";
 import Cart from "./components/Cart";
 import Home from "./components/Home";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "", element: <Navigate to="home" /> },
-      { path: "home", element: <Home /> },
-      { path: "shop", element: <Shop /> },
-      { path: "cart", element: <Cart /> },
-    ],
-  },
-]);
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <App />
+      <Routes>
+        <Route path="/" element={<Navigate to="home" />} />
+        <Route path="home" element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>
 );
